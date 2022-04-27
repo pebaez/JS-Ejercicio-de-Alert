0df5c37526255c755ae87ab6a958a0b06879e754
pebaez <90210722+pebaez@users.noreply.github.com>
2022-04-27 17:12:01 -0400

    v7
    sdfsdf

--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,4 @@
-swal("Oops!", "Bienvenidos al Sitio Web de Agricotech ! \n" + " La fecha actual es " + Date(), "success");
+//swal("Oops!", "Bienvenidos al Sitio Web de Agricotech ! \n" + " La fecha actual es " + Date(), "success");
 
 function generarNuevoColor()
 {
@@ -14,14 +14,76 @@ function generarNuevoColor()
 //	document.getElementById("hexadecimal").innerHTML = color;
 //	document.getElementById("text").innerHTML = "Copiar Color";
 
+	
+
+//alert(document.body.style.background);
+
 
 }
 
 
-setInterval(() => {
-    
-    generarNuevoColor();
+function isNumber(evt)
+	{
+		evt = (evt) ? evt : window.event;
+		var charCode = (evt.which) ? evt.which : evt.keyCode;
+		if ( (charCode > 31 && charCode < 48) || charCode > 57) {
+			return false;
+		}
+		return true;
+	}
 
-}, 2000);
+
+	function validateEmail(sEmail) {
+		var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+		if (filter.test(sEmail)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+function ValidaFormulario()
+	{
+
+		var error="";
+	
+
+		if ($('#nombre').val()=="") { error = error + " Debe Ingresar el nombre \n" };
+
+			
+		if ($('#telefono').val()=="") { error = error + " Debe Ingresar el telefono \n" };	
+
+	
+
+		var sEmail = $('#email').val();
+
+		
+			if (validateEmail(sEmail)==false) { error = error + "El email es incorrecto \n" };
+		
+
+		if ($('#comentario').val()=="") { error = error + " Debe Ingresar el comentario \n" };
+
+		if (error!="")
+		{
+			
+			swal("Oops!", error, "error");
+		
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+
+
+	}
+
+
+// setInterval(() => {
+    
+//     generarNuevoColor();
+
+// }, 2000);
 
   
